a50011199047f763adc275e039be9fc78b0a480c
Aetherinox <aetherinox@proton.me>
2023-10-10 21:22:23 -0700

    gpg overwrite

--- setup.sh
+++ setup.sh
@@ -1965,14 +1965,14 @@ function fn_app_nodejs()
         sleep 1
         if [ -z "${OPT_DEV_NULLRUN}" ]; then
             sudo mkdir -p /etc/apt/keyrings
-            curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+            curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg >> $LOGS_FILE 2>&1
         fi
         echo -e "[ ${STATUS_OK} ]"
 
         printf '%-57s %-5s' "    |--- Adding Repo Source" ""
         sleep 1
         if [ -z "${OPT_DEV_NULLRUN}" ]; then
-            echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+            echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list >> $LOGS_FILE 2>&1
         fi
         echo -e "[ ${STATUS_OK} ]"
 
